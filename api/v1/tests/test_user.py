995fecafd582f61b03ce06740d1a8d0ce329d212
Glen Baker <iepathos@gmail.com>
2016-12-03 21:43:21 -0500

    Added test to make sure wrong answers on user update show up in errors

--- api/v1/tests/test_user.py
+++ api/v1/tests/test_user.py
@@ -8,6 +8,7 @@ from main.models import User
 from main.tests.fixture import Fixture1
 from .factories import AccountTypeRiskProfileGroupFactory, ClientFactory, \
     GroupFactory, UserFactory, SecurityAnswerFactory
+import json
 
 
 class UserTests(APITestCase):
@@ -74,8 +75,8 @@ class UserTests(APITestCase):
         # the user must be a client, advisor or possibly supportstaff here, otherwise 403
         client = ClientFactory(user=self.user)
         client.user.groups_add(User.GROUP_CLIENT)
-        sa1 = SecurityAnswerFactory.create(user=client.user, question='question one')
-        sa2 = SecurityAnswerFactory.create(user=client.user, question='question two')
+        sa1 = SecurityAnswerFactory.create(user=self.user, question='question one')
+        sa2 = SecurityAnswerFactory.create(user=self.user, question='question two')
 
         url = reverse('api:v1:user-me')
         new_name = 'Bruce Wayne'
@@ -108,6 +109,36 @@ class UserTests(APITestCase):
         self.assertEqual(control_response.data, response.data)
         self.assertEqual(response.data['first_name'], new_name)
         self.assertEqual(response.data['id'], self.user.id)
+
+    def test_update_user_settings_bad_question_answers(self):
+        # the user must be a client, advisor or possibly supportstaff here, otherwise 403
+        client = ClientFactory(user=self.user)
+        client.user.groups_add(User.GROUP_CLIENT)
+        sa1 = SecurityAnswerFactory.create(user=self.user, question='question one')
+        sa2 = SecurityAnswerFactory.create(user=self.user, question='question two')
+
+        url = reverse('api:v1:user-me')
+        new_name = 'Bruce Wayne'
+        data = {
+            'first_name': new_name,
+            'last_name': self.user.last_name,
+            'email': self.user.email,
+            'password': 'test',
+            'password2': 'test',
+            'oldpassword': 'test',
+            'question_one': sa1.pk,
+            'answer_one': 'wrong answer',
+            'question_two': sa2.pk,
+            'answer_two': 'test',
+        }
+
+        self.client.force_authenticate(self.user)
+        response = self.client.put(url, data)
+        # 400 for put request
+        content = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         msg='400 for wrong answer_one with put request to update user settings')
+        self.assertEqual(content['error']['errors']['answer_one'], ['Wrong answer'])
 
     def test_phone_number_valid(self):
         url = reverse('api:v1:phonenumber-validation')
