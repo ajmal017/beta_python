dbc02211c66fb0aca5ca002ac593029143303d14
Glen Baker <iepathos@gmail.com>
2016-12-11 23:28:25 -0500

    Merged migrations

---
+++ client/migrations/0040_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('client', '0039_accountbeneficiary'),
+        ('client', '0039_auto_20161208_2218'),
+    ]
+
+    operations = [
+    ]
