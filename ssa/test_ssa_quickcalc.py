43e0845995633a0deb323e39df0d515c345d9617
Glen Baker <iepathos@gmail.com>
2016-12-16 19:20:48 -0500

    Added comment to test ssa quickcalc

--- ssa/test_ssa_quickcalc.py
+++ ssa/test_ssa_quickcalc.py
@@ -8,6 +8,8 @@ def test_ssa_quickcalc():
     get_social_security_benefit(earnings, last_earn, last_year_earn,
                                 retire_month, retire_year, dob_month,
                                 dob_day, dob_year)
+
+    should return the estimated retirement age, date, and amount
     """
     ret_age, ret_date, ret_amount = get_social_security_benefit(100000, 80000, 2014,
                                                                 1, 2048, 4, 30, 1986)
