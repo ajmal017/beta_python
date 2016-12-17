735bf885e07d65fff5fc53e9f079d8bfd5b6ca70
Glen Baker <iepathos@gmail.com>
2016-12-16 19:06:11 -0500

    Added initial functions to integrate with ssa quickcalc

---
+++ ssa/ssa.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import logging
+logger = logging.getLogger('ssa.ssa')
+
+
+def parse_retirement_paragraph(soup):
+    ret_age = int(soup.find(id='ret_age').split(' ')[0])
+    ret_date = int(soup.find(id='ret_date').strip(' '))
+    ret_amount = float(soup.find(id='ret_amount').strip(' '))
+    return ret_age, ret_date, ret_amount
+
+
+def ssa_quickcalc_soup(earnings, last_earn, last_year_earn,
+                       retire_month, retire_year, dob_month,
+                       dob_day, dob_year):
+    url = 'https://www.ssa.gov/cgi-bin/benefit6.cgi'
+    data = {
+        'earnings': earnings,
+        'retiremonth': retire_month,
+        'retireyear': retire_year,
+        'lastEarn': last_earn,
+        'lastYearEarn': last_year_earn,
+        'dobmon': dob_month,  # dob month
+        'dobday': dob_day,  # dob day
+        'yob': dob_year,  # dob year
+
+    }
+    r = requests.post(url, data)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+
+def get_social_security_benefit(earnings, last_earn, last_year_earn,
+                                retire_month, retire_year, dob_month,
+                                dob_day, dob_year):
+    soup = ssa_quickcalc_soup(earnings, last_earn, last_year_earn,
+                              retire_month, retire_year, dob_month,
+                              dob_day, dob_year)
+    ret_age, ret_date, ret_amount = parse_retirement_paragraph(soup)
+    return ret_age, ret_date, ret_amount
