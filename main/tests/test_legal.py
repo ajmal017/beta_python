b8947a250cfecd9b6a8f425d2d22e468310167e3
Ken Abe <ken.abe.1992@outlook.com>
2017-01-11 23:11:55 +0900

    BB-605 Update footer & add privacy notice, terms of use pages.

--- main/tests/test_legal.py
+++ main/tests/test_legal.py
@@ -6,12 +6,12 @@ from rest_framework import status
 
 class PrivacyPolicyTests(TestCase):
     def test_get_privacy_policy(self):
-        url = reverse('privacy_policy')
+        url = reverse('privacy_notice')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_online_privacy(self):
-        url = reverse('online_privacy')
+        url = reverse('terms_of_use')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
