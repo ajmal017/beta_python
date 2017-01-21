5665bb5caebaed5c3a0f88859d4d0cf36797ecb5
Abattia <ben.vanevery@gmail.com>
2017-01-20 19:14:56 -0500

    US taxes with income & network dynamic

--- api/v1/retiresmartz/views.py
+++ api/v1/retiresmartz/views.py
@@ -560,6 +560,16 @@ equired to generate the
         # Get US tax projection
         ira_rmd_factor = 26.5
         # These ones are fudged ...
+        house_value = 250000.
+        retire_earn_at_fra = 3490.
+        retire_earn_under_fra = 1310.
+        other_income=40000.
+        after_tax_income = 110982.
+        federal_taxable_income = 109996.
+        federal_regular_tax = 20614.
+        ss_fra_retirement = 7002.
+        paid_days = 2
+        initial_401k_balance = 50000
         risk_profile = 0.5
         federal_regular_tax = 20614
         employee_contributions_last_year = 0.055
@@ -575,20 +585,20 @@ equired to generate the
                         plan.client.life_expectancy,
                         plan.lifestyle,
                         plan.reverse_mortgage,
-                        plan.client.home_value,
+                        house_value,
                         plan.client.civil_status,
-                        plan.client.ss_fra_retirement,
-                        plan.client.ss_fra_todays,
+                        retire_earn_at_fra,
+                        retire_earn_under_fra,
                         plan.client.income,
                         plan.client.net_worth,
                         plan.client.income,
                         federal_regular_tax,
-                        plan.atc,
-                        plan.client.other_income,
-                        plan.client.ss_fra_retirement,
-                        plan.paid_days,
+                        after_tax_income,
+                        other_income,
+                        ss_fra_retirement,
+                        paid_days,
                         ira_rmd_factor,
-                        plan.balance,
+                        initial_401k_balance,
                         inflation.inflation_level,
                         risk_profile,
                         plan.income_growth,
