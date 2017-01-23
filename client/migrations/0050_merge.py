cbfafff6ca96e7e61b06dace3e3120af2e89d959
Glen Baker <iepathos@gmail.com>
2017-01-23 14:39:33 -0500

    Added migration merge

---
+++ client/migrations/0050_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('client', '0049_emailinvite_partner_social_security_statement'),
+        ('client', '0049_auto_20170119_1651'),
+    ]
+
+    operations = [
+    ]
