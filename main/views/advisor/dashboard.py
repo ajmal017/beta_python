5a0186b390b3d4dc01a6f0689bd2623d872e554e
Leon <leon.verspeek@hotmail.com>
2017-02-07 23:43:47 +0100

    BB-506 fix advisor overview

--- main/views/advisor/dashboard.py
+++ main/views/advisor/dashboard.py
@@ -414,15 +414,18 @@ class AdvisorCompositeOverview(ListView, AdvisorView):
     def get_context_data(self, **kwargs):
         ctx = super(AdvisorCompositeOverview, self).get_context_data(
             **kwargs)
-        ctx["groups"] = self.get_queryset()
+        groups = self.get_queryset()
+        ctx["groups"] = groups
 
         pre_clients = Client.objects.filter(user__prepopulated=False,
                                             advisor=self.advisor)
-        clients = []
+        clients = set(pre_clients)
 
         for client in set(pre_clients.distinct().all()):
-            if not client.accounts.count():
-                clients.append(client)
+            for group in set(groups.distinct().all()):
+                for account in set(group.accounts.distinct().all()):
+                    if account.primary_owner.id == client.pk:
+                        clients.remove(client)
 
         ctx['clients'] = clients
         return ctx
