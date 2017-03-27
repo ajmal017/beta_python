a81f77fecd10e22965a2b48ce74d07a932cc72a0
Glen Baker <iepathos@gmail.com>
2017-03-27 17:46:13 -0400

    Merged conflicting client migrations

---
+++ client/migrations/0061_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('client', '0060_auto_20170328_0304'),
+        ('client', '0060_auto_20170328_0710'),
+    ]
+
+    operations = [
+    ]
